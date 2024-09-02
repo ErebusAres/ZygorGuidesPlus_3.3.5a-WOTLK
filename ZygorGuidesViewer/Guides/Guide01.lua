b5a150749ed4b4e5a03f51f4f6fdc49c0d08bebb
ErebusAres <erebusares@gmail.com>
2024-09-02 17:19:27 -0500

    Update Guide01.lua

--- ZygorGuidesViewer/Guides/Guide01.lua
+++ ZygorGuidesViewer/Guides/Guide01.lua
@@ -6,7 +6,7 @@ if UnitFactionGroup("player")~="Alliance" then return end
 ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Exploration Achievements\\Kalimdor Progress",[[
     description This guide section displays the entire progress for Kalimdor.
     author ErebusAres
-    next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Teldrassil
+    next Corey's Achievement Guides\\Explore Kalimdor\\Explore Teldrassil
     step //1
         .'Explore Kalimdor Total Progress |achieve 43
     step //2
@@ -33,12 +33,12 @@ ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Exploration Achieve
         .'Progress for Winterspring |achieve 857
 
     ]])
-ZygorGuidesViewer:RegisterGuide("Coreys's Achievement Guides\\Explore Kalimdor\\Explore Teldrassil",[[
+ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Explore Kalimdor\\Explore Teldrassil",[[
 	author ErebusAres
-    next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Darkshore
+    next Corey's Achievement Guides\\Explore Kalimdor\\Explore Darkshore
 	step //1
-        goto Teldrassil,55,91
-        .'Unlock Rut'theran Village|achieve 842/6
+        .'Unlock Rut'theran Village|achieve 842/6|goto Teldrassil,55,91
+        .'Begin in Darnassus|goto Darnassus|noway|c
     step //2
         goto Darnassus,25,55
         .'Unlock Darnassus|achieve 842/11
@@ -69,19 +69,30 @@ ZygorGuidesViewer:RegisterGuide("Coreys's Achievement Guides\\Explore Kalimdor\\
     step //11
         goto Teldrassil,38,30
         .'Unlock The Oracle Glade|achieve 842/8
-]])
-ZygorGuidesViewer:RegisterGuide("Coreys's Achievement Guides\\Explore Kalimdor\\Explore Darkshore",[[
-author ErebusAres
-next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Ashenvale
-    step //1
-        .'Head towards Darkshore through Darnassus|goto Darnassus|noway|c
-    step //2
+    step //12
+        .'Return to Darnassus|goto Darnassus|noway|c
+    step //13
         .'Go through the pink portal towards Rut'theran Village|goto Darnassus,30.8,41.4
+    step //14
+        .'Unlock Rut'theran Village|achieve 842/6|goto Teldrassil,55,91
+    step //15
+        goto Teldrassil,58.4,94
+        .talk Vesprystus##3838
+        ..fpath Rut'theran Village
+]])
+ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Explore Kalimdor\\Explore Darkshore",[[
+author ErebusAres
+next Corey's Achievement Guides\\Explore Kalimdor\\Explore Ashenvale
     step //3
-        .'Get on the boat to Darkshore|goto Teldrassil,54.9,96.8
+        .'Boat to Darkshore|goto Teldrassil,54.9,96.8|or
+        .'Fly to Darkshore|goto Darkshore|noway|c|or
     step //4
         goto Darkshore,38,44
         .'Arrive and Unlock Auberdine|achieve 844/1
+    step //5
+        goto Darkshore,36.3,45.6
+        .talk Caylais Moonfeather##3841
+        ..fpath Auberdine
     step //5
         goto Darkshore,44,36
         .'Unlock Bashal'Aran|achieve 844/5
@@ -107,9 +118,9 @@ next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Ashenvale
         goto Darkshore,38,86
         .'Unlock The Master's Glaive|achieve 844/9
 ]])
-ZygorGuidesViewer:RegisterGuide("Coreys's Achievement Guides\\Explore Kalimdor\\Explore Ashenvale",[[
+ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Explore Kalimdor\\Explore Ashenvale",[[
 author ErebusAres
-next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Felwood
+next Corey's Achievement Guides\\Explore Kalimdor\\Explore Felwood
     step //1
         .'Head southeast to Ashenvale|goto Ashenvale|noway|c
     step //2
@@ -157,9 +168,9 @@ next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Felwood
     step //21
         .'Unlock The Howling Vale|achieve 845/11|goto Ashenvale,54,36|tip Go up the path and enter the entrance of the cave at the top.
 ]])
-ZygorGuidesViewer:RegisterGuide("Coreys's Achievement Guides\\Explore Kalimdor\\Explore Felwood",[[
+ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Explore Kalimdor\\Explore Felwood",[[
 author ErebusAres
-next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Moonglade
+next Corey's Achievement Guides\\Explore Kalimdor\\Explore Moonglade
     step //1
         .'Just stick to the road during most of this journey and deviate only when necessary.
         .'Unlock Morlos'Aran|achieve 853/12|goto Felwood,56,87
@@ -194,9 +205,9 @@ next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Moonglade
     step //14
         .'Unlock Felpaw Village|achieve 853/1|goto Felwood,63,10|tip After leaving the flight path, follow the path back north and turn right onto the main road and follow it north.
 ]])
-ZygorGuidesViewer:RegisterGuide("Coreys's Achievement Guides\\Explore Kalimdor\\Explore Moonglade",[[
+ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Explore Kalimdor\\Explore Moonglade",[[
 author ErebusAres
-next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Winterspring
+next Corey's Achievement Guides\\Explore Kalimdor\\Explore Winterspring
     step //1
         .'Continue following the road around towards the north towards Moonglade|goto Felwood,64.9,8.3|tip Through the Bear Head shaped cave.
     step //2
@@ -206,7 +217,8 @@ next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Winterspring
     step //4
         .'Unlock Lake Elune'ara|achieve 855/1|goto 37.6,53.4
     step //5
-        goto Moonglade,44.1,45.3|tip Continue following the path, into the building, and across the bridge.| only Druid
+        only Druid
+        goto Moonglade,44.1,45.3|tip Continue following the path, into the building, and across the bridge.
         .talk Silva Fil'naveth##11800
         ..fpath Moonglade
     step //6
@@ -214,9 +226,9 @@ next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Winterspring
         .talk Sindrayl##10897
         ..fpath Moonglade
 ]])
-ZygorGuidesViewer:RegisterGuide("Coreys's Achievement Guides\\Explore Kalimdor\\Explore Winterspring",[[
+ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Explore Kalimdor\\Explore Winterspring",[[
 author ErebusAres
-next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Azshara
+next Corey's Achievement Guides\\Explore Kalimdor\\Explore Azshara
     step //1
         .'Return back into the cave.|goto Felwood|noway|c
     step //2
@@ -256,29 +268,163 @@ next Coreys's Achievement Guides\\Explore Kalimdor\\Explore Azshara
     step //20
         .'Unlock Darkwhisper Gorge|achieve 857/13|goto Winterspring,58,80
 ]])
-ZygorGuidesViewer:RegisterGuide("Coreys's Achievement Guides\\Explore Kalimdor\\Explore Azshara",[[
+ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Explore Kalimdor\\Explore Azshara",[[
 author ErebusAres
-next Coreys's Achievement Guides\\Explore Kalimdor\\Explore The Barrens
+next Corey's Achievement Guides\\Explore Kalimdor\\Explore The Barrens
     step //1
-        .'Unlock Bay of Storms|achieve 852/1 |goto Azshara,56,59
-        .'Unlock The Shattered Strand|achieve 852/2 |goto Azshara,45,52 
-        .'Unlock Jagged Reef|achieve 852/3 |goto Azshara,52,10
-        .'Unlock Bitter Reaches|achieve 852/4 |goto Azshara,73,21
-        .'Unlock Tower of Eldara|achieve 852/5 |goto Azshara,89,33
-        .'Unlock Temple of Arkkoran|achieve 852/6 |goto Azshara,76,43
-        .'Unlock Legash Encampment|achieve 852/7 |goto Azshara,51,19
-        .'Unlock Thalassian Base Camp|achieve 852/8 |goto Azshara,56,29 
-        .'Unlock Ursolan|achieve 852/9 |goto Azshara,44,27
-        .'Unlock Timbermaw Hold|achieve 852/10 |goto Azshara,38,33 
-        .'Unlock Valormok|achieve 852/11 |goto Azshara,21,51
-        .'Unlock Haldarr Encampment|achieve 852/12 |goto Azshara,20,61
+        .'Fly to Forest Song, Ashenvale|goto Ashenvale|noway|c
+    step //2
+        .'Travel East to Azshara|goto Azshara|noway|c
+    step //3
+        goto Southfury River,11.9,77.6
+        .talk Jarrodenus##12577
+        ..fpath Talrendis Point
+    step //4
         .'Unlock Shadowsong Shrine|achieve 852/13 |goto Azshara,13,74
-        .'Unlock Ruins of Eldarath|achieve 852/14 |goto Azshara,36,53
+    step //5
+        .'Go up the path starting here|goto Azshara,26.7,61.1
+    step //6
+        .'Unlock Forlorn Ridge|achieve 852/19 |goto Azshara,28,9
+    step //7
+        .'Unlock Lake Mennar|achieve 852/17 |goto Azshara,40,80|tip Stay on the North side of the Forlorn Ridge's Mountain. Then, head South through the square arch.
+    step //8
+        .'Unlock The Ruined Reaches|achieve 852/18 |goto Azshara,51,88|tip Take care not to fall off the cliff. Hug the edge and you will unlock the region.
+    step //9
+        .'Path North to this point|goto Azshara,51,84
+    step //10
+        .'Cross the Bridge and continue East across the second Bridge|goto Azshara,62.5,81.9
+    step //11
+        .'Unlock Ravencrest Monument|achieve 852/16 |goto Azshara,72,86|tip Follow the crevace around the North side, Head East and Cross the bridge.
+    step //10
         .'Unlock Southridge Beach|achieve 852/15 |goto Azshara,47,66
-        .'Unlock Ravencrest Monument|achieve 852/16 |goto Azshara,72,86
-        .'Unlock Lake Mennar|achieve 852/17 |goto Azshara,40,80
-        .'Unlock The Ruined Reaches|achieve 852/18 |goto Azshara,51,88
-        .'Unlock Forlorn Ridge|achieve 852/19 |goto Azshara,30,71 
+    step //11
+        .'Unlock The Shattered Strand|achieve 852/2 |goto Azshara,45,52
+    step //12
+        .'Unlock Bay of Storms|achieve 852/1 |goto Azshara,56,59
+    step //13
+        .'Unlock Temple of Arkkoran|achieve 852/6 |goto Azshara,76,43
+    step //14
+        .'Unlock Tower of Eldara|achieve 852/5 |goto Azshara,89,33|tip Head down the beach towards the tower by the water.
+    step //15
+        .'Head NorthWest up the Land ramp|goto Azshara,84,27
+    step //16
+        .'Unlock Bitter Reaches|achieve 852/4 |goto Azshara,73,21
+    step //17
+        .'Unlock Thalassian Base Camp|achieve 852/8 |goto Azshara,56,29 
+    step //18
+        .'Unlock Jagged Reef|achieve 852/3 |goto Azshara,52,10|tip Be careful not to fall off the cliff. Hug the edge and you will unlock the region.
+    step //19
+        .'Unlock Legash Encampment|achieve 852/7 |goto Azshara,51,19
+    step //20
+        .'Unlock Ursolan|achieve 852/9 |goto Azshara,44,27
+    step //21
+        .'Unlock Timbermaw Hold|achieve 852/10 |goto Azshara,38,33 
+    step //22
+        .'Unlock Ruins of Eldarath|achieve 852/14 |goto Azshara,36,53
+    step //23
+        .'Unlock Valormok|achieve 852/11 |goto Azshara,21,51
+    step //24
+        .'Unlock Haldarr Encampment|achieve 852/12 |goto Azshara,20,61
+]])
+ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Explore Kalimdor\\Explore The Barrens",[[
+author ErebusAres
+next Corey's Achievement Guides\\Explore Kalimdor\\Explore Durotar
+    step //1
+        .'Travel or Fly to The Barrens|goto The Barrens|noway|c
+    step 
+        .'Unlock The Mor'shan Rampart|achieve 750/3 |goto The Barrens,49,5
+    step
+        .'Unlock Dreadmist Peak|achieve 750/4 |goto The Barrens,47,17
+    step
+        .'Unlock The Dry Hills|achieve 750/5 |goto The Barrens,39,14
+    step
+        .'Unlock The Forgotten Pools|achieve 750/6 |goto The Barrens,45,22
+    step
+        .'Unlock Honor's Stand|achieve 750/7 |goto The Barrens,38,28|tip Head to towards the Watchtower South of the Mountain.
+    step
+        .'Unlock Lushwater Oasis|achieve 750/17 |goto The Barrens,46,39
+    step
+        .'Unlock Agama'gor|achieve 750/18 |goto The Barrens,43,48
+    step
+        .'Unlock Camp Taurajo|achieve 750/20 |goto The Barrens,45,60
+    step
+        .'Unlock Field of Giants|achieve 750/21 |goto The Barrens,47,70
+    step
+        .'Unlock Blackthorn Ridge|achieve 750/22 |goto The Barrens,43,79
+    step
+        .'Unlock Bael Modan|achieve 750/23 |goto The Barrens,47,84
+    step
+        .'Unlock Razorfen Downs|achieve 750/25 |goto The Barrens,47,90
+    step
+        .'Unlock Razorfen Kraul|achieve 750/24 |goto The Barrens,41,86
+    step
+        .'Unlock Bramblescar|achieve 750/19 |goto The Barrens,51,54
+    step
+        .'Unlock The Stagnant Oasis|achieve 750/12 |goto The Barrens,55,42
+    step
+        .'Unlock Raptor Grounds|achieve 750/16 |goto The Barrens,57,53
+    step
+        .'Unlock Northwatch Hold|achieve 750/15 |goto The Barrens,60,54
+    step
+        .'Unlock The Merchant Coast|achieve 750/14 |goto The Barrens,63,43|tip Follow the 'path' around the South side of the ridge towards the coast, then head North when able.
+    step
+        .'Unlock Ratchet|achieve 750/13 |goto The Barrens,62,37|tip Keep going North, past the large ship, into the city of Ratchet.
+    step
+        goto The Barrens,63.1,37.2
+        .talk Bragok##16227
+        ..fpath Ratchet
+    step
+        .'Unlock The Crossroads|achieve 750/11 |goto The Barrens,51,28|tip Leave the city via the Main road Follow it NorthWest.
+    step
+        .'Unlock Thorn Hill|achieve 750/10 |goto The Barrens,57,28|tip Keep to the North of the Mountains and enter Thorn Hill area.
+    step
+        .'Unlock Grol'dom Farm|achieve 750/8 |goto The Barrens,55,20
+    step
+        .'Unlock The Sludge Fen|achieve 750/2 |goto The Barrens,56,7|tip Keep to the West of the Farm and Continue Northward.
+    step
+        .'Unlock Boulder Lode Mine |achieve 750/1 |goto The Barrens,61,4|tip Keep Heading straight East until you reach the small road, then nab the Region.
+    step
+        .'Unlock Far Watch Post|achieve 750/9 |goto The Barrens,61,21|tip Follow the small road South to get this Region.
+]]) 
+ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Explore Kalimdor\\Explore Durotar",[[
+author ErebusAres
+next Corey's Achievement Guides\\Explore Kalimdor\\Explore Stonetalon Mountains
+    step
+        .'Travel to Durotar|goto Durotar|noway|c
+    step
+        .'Unlock Razormane Grounds |achieve 728/7 |goto Durotar,41,45
+    step
+        .'Unlock Razor Hill |achieve 728/6 |goto Durotar,53,43
+    step
+        .'Unlock Tiragarde Keep |achieve 728/5 |goto Durotar,57,54
+    step
+        .'Unlock Sen'jin Village |achieve 728/2 |goto Durotar,55,74
+    step
+        .'Unlock Echo Isles |achieve 728/4 |goto Durotar,62,81|tip This should unlock halfway to the first set of small islands.
+    step
+        .'Unlock Kolkar Crag |achieve 728/3 |goto Durotar,48,78
+    step
+        .'Travel to the Entrance of Valley of Trials|goto Durotar,50.6,68.4|tip WARNING, WARNING, WARNING: This area is 65+
+    step
+        .'Unlock Valley of Trials |achieve 728/1 |goto Durotar,44,59|tip Just peek your head in and the Region should unlock.
+    step
+        .'Travel the road Back North|goto Durotar,52.6,45.4
+    step
+        .'Unlock Thunder Ridge |achieve 728/8 |goto Durotar,39,28|tip Follow the road West, then head North towards the Region.
+    step
+        .'Unlock Drygulch Ravine |achieve 728/9 |goto Durotar,53,23
+    step
+        .'Unlock Skull Rock |achieve 728/10 |goto Durotar,54,13
+    step
+        .'Prepare to Enter Orgrimmar |goto Durotar,45.7,13.3|tip WARNING WARNING WARNING: THIS AREA CONTAINS HIGH LEVEL HORDE ENEMIES! PROCEED WITH CAUTION.
+    step
+        .'Unlock Orgrimmar |achieve 728/11|tip Quickly run in and leave when you get the achievement.
+]])
+ZygorGuidesViewer:RegisterGuide("Corey's Achievement Guides\\Explore Kalimdor\\Explore Stonetalon Mountains",[[
+author ErebusAres
+next Corey's Achievement Guides\\Explore Kalimdor\\Explore Stonetalon Mountains
+    step
+        .' Not started yet :(
 ]])
 --TRIAL ZygorGuidesViewer:RegisterGuide("Zygor's Alliance Leveling Guides\\Main Guide (13-20)",[[
 --TRIAL 	author support@zygorguides.com
